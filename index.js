376f405785c20db1e4709ebe632d3934fb50f946
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-11-25 01:10:12 -0600

    asked engineer questions

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const inquirer = require("inquirer");
 
 function whatOption(answers) {
-    if(answers.engineerOrInternOrTeam === 'engineer') {
-        console.log('chose engineer');
-        // This logged that i chose engineer
-        // need to add inquirer prompt here to ask the questions based on
-        // what option was chosen!
-    }
+  if (answers.engineerOrInternOrTeam === "engineer") {
+    console.log("chose engineer");
+    // This logged that i chose engineer
+    // need to add inquirer prompt here to ask the questions based on
+    // what option was chosen!
+
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the team engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "engineerGitHub",
+        message: "What is the engineer's GitHub Username?",
+      },
+    ]);
+  }
 }
 
 const promptUser = () => {
@@ -37,12 +60,11 @@ const promptUser = () => {
       message:
         "Would you like to add an engineer, add an intern, or finish building your team?",
       choices: ["engineer", "intern", "finish builing the team"],
-    }
+    },
   ]);
 };
 
-promptUser()
-.then(answers => {
-    whatOption(answers);
-    console.log(answers);
+promptUser().then((answers) => {
+  whatOption(answers);
+//   console.log(answers); // logs answer object to promptUser questions
 });
